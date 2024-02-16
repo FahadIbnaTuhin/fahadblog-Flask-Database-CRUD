964cac37e6fdc95d83338135738a1548de2bf714
FahadIbnaTuhin <fahadtuhin2@gmail.com>
2024-02-16 16:50:39 +0600

    app.py

--- app.py
+++ app.py
@@ -246,6 +246,6 @@ def delete_post(post_id):
     return redirect(url_for('get_all_posts'))
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
-    # app.run(host='0.0.0.0', port=5000)
+# if __name__ == "__main__":
+#     app.run(debug=True)
+#     # app.run(host='0.0.0.0', port=5000)
